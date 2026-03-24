b0d54e6ca8d9ed0ee5163564f6c3fbcb4689eaa8
Michael <134744264+FordEx18@users.noreply.github.com>
2026-03-24 03:34:31 +0000

    Added Methods for Observer Pattern

--- week4-exercises/mini08-observer-pattern/src/main/java/edu/norcocollege/cis18b/weekx/mini08/ObserverApp.java
+++ week4-exercises/mini08-observer-pattern/src/main/java/edu/norcocollege/cis18b/weekx/mini08/ObserverApp.java
@@ -5,6 +5,10 @@ public class ObserverApp {
         AlertService service = new AlertService();
 
         // TODO: Register at least two observers.
+        service.addObserver(new LogAlertObserver());
+        service.addObserver(new EmailAlertObserver());
         // TODO: Process one alert.
+        Alert alert = new Alert(001,"Test alert message", AlertLevel.CRITICAL);
+        service.processAlert(alert);
     }
 }
