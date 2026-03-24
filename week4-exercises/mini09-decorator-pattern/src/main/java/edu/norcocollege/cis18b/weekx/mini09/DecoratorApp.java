536f89ad2a61676b524d18a63bfc77e1c2757d62
Michael <134744264+FordEx18@users.noreply.github.com>
2026-03-24 03:46:17 +0000

    Added Methods for decorator Pattern

--- week4-exercises/mini09-decorator-pattern/src/main/java/edu/norcocollege/cis18b/weekx/mini09/DecoratorApp.java
+++ week4-exercises/mini09-decorator-pattern/src/main/java/edu/norcocollege/cis18b/weekx/mini09/DecoratorApp.java
@@ -3,9 +3,11 @@ package edu.norcocollege.cis18b.weekx.mini09;
 public class DecoratorApp {
     public static void main(String[] args) {
         Alert alert = new Alert(1, "CPU usage exceeded threshold", AlertLevel.CRITICAL);
-
         // TODO: Create a BasicAlertHandler.
+            AlertHandler handler = new BasicAlertHandler();
         // TODO: Wrap it in LoggingAlertHandlerDecorator.
+        handler = new LoggingAlertHandlerDecorator(handler);
         // TODO: Handle the alert.
+        handler.handle(alert);
     }
 }
--- week4-exercises/mini09-decorator-pattern/src/main/java/edu/norcocollege/cis18b/weekx/mini09/LoggingAlertHandlerDecorator.java
+++ week4-exercises/mini09-decorator-pattern/src/main/java/edu/norcocollege/cis18b/weekx/mini09/LoggingAlertHandlerDecorator.java
@@ -10,7 +10,10 @@ public class LoggingAlertHandlerDecorator implements AlertHandler {
     @Override
     public void handle(Alert alert) {
         // TODO: Print a message before delegating.
+        System.out.println("Logging: Handling alert with message: " + alert.message());
         // TODO: Call the wrapped handler.
+        wrapped.handle(alert);
         // TODO: Print a message after delegating.
+        System.out.println("Logging: Finished handling alert.");
     }
 }
